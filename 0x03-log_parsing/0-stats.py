fa36dbba21091e3fc99321a6aefaba069444aba8
Dev-Nonso <chinonsoogbonnaya47@gmail.com>
2024-11-03 08:32:43 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,29 +14,28 @@ def initialize_log():
     return log
 
 
-def parse_line():
+def parse_line(line, regex, log):
     match = regex.fullmatch(line)
     if match:
-        status_code, file_size * match.group(1,2)
-        log["file_size"] += int(file_size)
-        if stat_code.isdecimal():
-            log["code_list"][stat_code] += 1
+        status_code, file_size = match.groups()  
+        log["file_size"] += int(file_size) 
+        if status_code.isdecimal():
+            log["code_list"][status_code] += 1
     return log
 
 
-
-def print_codes():
+def print_codes(log):
     print("file size: {}".format(log["file_size"]))
     sorted_code_list = sorted(log["code_list"])
     for code in sorted_code_list:
         if log["code_list"][code]:
-            print(f"{code}: {log["code_list"][code]}")
+            print(f"{code}: {log['code_list'][code]}")
 
 
 
 
 def main():
-    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
 
     log = initialize_log()
 
@@ -44,12 +43,14 @@ def main():
 
     for line in sys.stdin:
         line = line.strip()
-        line_count = line_count + 1
-        parsed_log = parse_line(line, regex, log)
+        line_count += 1
+        log = parse_line(line, regex, log)
         if line_count % 10 == 0:
-            print_codes(parsed_log)
+            print_codes(log)
 
+    print_codes(log)
 
 
 if __name__ == "__main__":
     main()
+
